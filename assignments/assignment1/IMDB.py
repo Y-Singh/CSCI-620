295957f15873c63b0999f65c1e36eea60463b249
Y-Singh <=>
2020-09-13 01:24:07 -0400

    Created method to add all workers in one func

--- assignments/assignment1/IMDB.py
+++ assignments/assignment1/IMDB.py
@@ -1,18 +1,20 @@
-import psycopg2
+# import psycopg2
+from psycopg2 import errors, sql, connect
 import csv
 import os
-
+import re
 from pdb import set_trace
 
 class IMDB_DB:
     def __init__(self, path):
-        self.conn = psycopg2.connect(
-            database="postgres", user='postgres', password='admin', host='127.0.0.1', port='5432'
+        self.conn = connect(
+            database="imdb", user='postgres', password='admin', host='127.0.0.1', port='5432'
         )
-        self.cur = self.conn.cursor(prepared=True)
+        self.cur = self.conn.cursor()
         self.path = path
         self.movies_query = []
         self.actors_query = []
+        self.directors_query = []
 
     def read_movies_basic(self):
         tsv_file = open(path+"\\title.basics.tsv\\data.tsv", 'r', encoding='utf-8')
@@ -20,41 +22,20 @@ class IMDB_DB:
         for row in movies_tsv:
             if row['titleType'] == "movie":
                 if row['isAdult'] == '0':
-                    print(row.values())
-                    genres = row['genres'].split(',')
+                    params = row
+                    genres = params['genres'].split(',')
                     gs = [None] * 3
                     for i in range(0, len(genres)):
                         gs[i] = genres[i]
-                    row.pop('genres', None)
-                    row['genre_one'] = gs[0]
-                    row['genre_two'] = gs[1]
-                    row['genre_three'] = gs[2]
-                    row['tconst'] = row['tconst'][2:]
-                    self.create_movie_tmp(row)
-            
-        print(len(self.querys))
-        print(self.querys[-1][1])
-        
+                    params.pop('genres', None)
+                    params['genre_one'] = gs[0]
+                    params['genre_two'] = gs[1]
+                    params['genre_three'] = gs[2]
+                    params['tconst'] = row['tconst'][2:]
+                    self.create_movie(params)
         tsv_file.close()
 
-    def create_movie(
-        self,
-        title_id:int, p_title:str, o_title:str, region:str, lang:str, release_yr:str, duration:int, 
-        ratings:float, votes:int, genre_one:str, genre_two:str=None, genre_three:str=None, 
-    ):
-
-        qry = f"""
-            INSERT INTO movies 
-            (title_id, primary_title, original_title, region, language, release_year, duration, ratings, votes, genre_one, genre_two, genre_three)
-            VALUES
-            ({title_id}, {p_title}, {o_title}, {region}, {lang}, {release_yr}, {duration}, {ratings}, {votes}, {genre_one}, {genre_two}, {genre_three})
-        """
-        if len(self.querys) < 100:
-            self.querys.append(qry)
-        else:
-            self.querys.clear()
-    
-    def create_movie_tmp(self,  params:dict):
+    def create_movie(self,  params:dict):
         qry = """
             INSERT INTO movies 
             (title_id, primary_title, original_title, runtime_minutes, genre_one, genre_two, genre_three)
@@ -68,26 +49,118 @@ class IMDB_DB:
         names_tsv = csv.DictReader(tsv_file, delimiter="\t", quoting=csv.QUOTE_NONE)
         for row in names_tsv:
             professions = row['primaryProfession'].split(',')
-            for job in professions:
-                if job.upper() == "ACTOR" or job.upper() == "ACTRESS":
-                    params = row
-                    self.create_actor_tmp(params)
-                elif job.upper() == "DIRECTOR":
-                    
+            professions = [job.upper() for job in professions]
+            jobs = self.check_actor_dups(professions)
+            params = dict()
+            name = row['primaryName'].split(" ", 1)
+            params['id'] = row['nconst'][2:]
+            params['first_name'] = name[0]
+            params['last_name'] = name[1]
+            params['birth_year'] = row['birthYear']
+            params['death_year'] = row['deathYear']
+            for job in jobs:
+                if job == "ACTOR" or job == "ACTRESS":
+                    params['table_name'] = 'actors'
+                    params['id_type'] = 'actor_id'
+                    self.create_showbiz_person(params)
+                elif job == "DIRECTOR":
+                    params['table_name'] = 'directors'
+                    params['id_type'] = 'director_id'
+                    self.create_showbiz_person(params)
+                elif job == "WRITER":
+                    params['table_name'] = 'writers'
+                    params['id_type'] = 'writer_id'
+                    self.create_showbiz_person(params)
+                elif job == "PRODUCERS":
+                    params['table_name'] = 'producers'
+                    params['id_type'] = 'producer_id'
+                    self.create_showbiz_person(params)
+    
+    def check_actor_dups(self, professions:list):
+        sub = "ACT"
+        x = [i for i in professions if sub in i]
+        if len(x) == 2:
+            professions.remove(x[-1])
+        return professions
+    
+    def check_params(self, params:dict):
+        for key, value in params.items():
+            if params[key] == "\\N":
+                params[key] = None
+        return params
 
+    def create_showbiz_person(self, params:dict):
+        params = self.check_params(params)
+        qry = sql.SQL(
+            """
+            INSERT INTO {table}
+            ({id_type}, first_name, last_name, birth_year, death_year)
+            VALUES
+            (%(id)s, %(first_name)s, %(last_name)s, %(birth_year)s, %(death_year)s);
+            """
+            ).format(
+                table=sql.Identifier(params['table_name']),
+                id_type=sql.Identifier(params['id_type'])
+                )        
+        try:
+            self.cur.execute(qry, params)
+        except errors.UniqueViolation as err:
+            print("[ERROR] : Actor already exists")
+            print(err)
+            self.conn.rollback()
+        else:
+            self.conn.commit()
 
-    def create_actor_tmp(self, params:dict):
+    def create_actor(self, params:dict):
+        params = self.check_params(params)
         qry = """
             INSERT INTO actors
             (actor_id, first_name, last_name, birth_year, death_year)
             VALUES
-            (%()s, %()s, %()s, %()s, %()s)
+            (%(id)s, %(first_name)s, %(last_name)s, %(birth_year)s, %(death_year)s);
         """
-        self.actors_query.append((qry, params))
+        try:
+            self.cur.execute(qry, params)
+        except errors.UniqueViolation as err:
+            print("[ERROR] : Actor already exists")
+            print(err)
+            self.conn.rollback()
+        else:
+            self.conn.commit()
     
+    def create_director(self, params:dict):
+        params = self.check_params(params)
+        qry = """
+            INSERT INTO directors
+            (director_id, first_name, last_name, birth_year, death_year)
+            VALUES
+            (%(id)s, %(first_name)s, %(last_name)s, %(birth_year)s, %(death_year)s);
+        """ 
+        try:
+            self.cur.execute(qry, params)
+        except errors.UniqueViolation as err:
+            print("[ERROR] : Actor already exists")
+            print(err)
+            self.conn.rollback()
+        else:
+            self.conn.commit()
 
-
-    
+    def create_writer(self, params:dict):
+        params = self.check_params(params)
+        qry = """
+            INSERT INTO writers
+            (writer_id, first_name, last_name, birth_year, death_year)
+            VALUES
+            (%(id)s, %(first_name)s, %(last_name)s, %(birth_year)s, %(death_year)s)
+        """ 
+        try:
+            self.cur.execute(qry, params)
+        except errors.UniqueViolation as err:
+            print("[ERROR] : Actor already exists")
+            print(err)
+            self.conn.rollback()
+        else:
+            self.conn.commit()
     
         
 
@@ -95,5 +168,5 @@ class IMDB_DB:
 if __name__ == "__main__":
     path = os.path.dirname(os.path.abspath(__file__))
     dB = IMDB_DB(path)
-    dB.read_movies_basic()
+    dB.read_names_basic()
     
